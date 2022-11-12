a5bc15f3f8f93db0f885d32ade088323ad689448
NastyaBormakova <bormakovaanas>
2022-11-12 14:45:14 +0300

    Работа с функциями, если несколько пользователей

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const userOne = {
     firstname: 'Настя',
 }
 
+const userTwo = {
+    role: 'manager',
+    firstname: 'Саша',
+}
+
 function checkAccess(user, role) {
     if (user.role === role){
         return true;
@@ -17,6 +22,25 @@ function showSuccessMessage (user) {
     console.log('################');
 }
 
+function showErrorMessage (user) {
+    console.log('!!!!!!!!!!!!!!!!!!!!!');
+    console.log(`! Доступ закрыт ${user.firstname} !`);
+    console.log('!!!!!!!!!!!!!!!!!!!!!');
+}
+
 if (checkAccess(userOne, 'admin')) {
     showSuccessMessage (userOne);
+} else {
+    showErrorMessage(userOne)
 }
+
+function checkAccessAndShowMessage(user, role) {
+    if (checkAccess(user, role)) {
+        showSuccessMessage(user);
+    } else {
+    showErrorMessage(user);
+    }
+}
+
+checkAccessAndShowMessage(userOne, 'admin')
+checkAccessAndShowMessage(userTwo, 'manager')
